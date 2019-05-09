2ee43be0a0423b4a85c7d0d7533efabb540907a8
Lucas Wagler <wagler.lucas@gmail.com>
2019-05-08 22:22:18 -0400

    add hidden wave toggle

--- public/main.js
+++ public/main.js
@@ -1,2 +1,7 @@
 console.log("    ______\n   /~/|/\\~\\\n   \\/ || \\/\n     //\n    ||\n     \\\\\n   ___\\\\_o____\n _/ ' .  .  . \\__\n/ .  .  '  . ' . \\", "_,--``--,_".repeat(3),"\n ");
 console.log("🌊🌊🌊 keep it wavy 🌊🌊🌊")
+
+function toggle_visibility(id) {
+    var element = document.getElementById(id);
+    element.style.visibility = (element.style.visibility == 'visible') ? 'hidden' : 'visible';
+}
